6490acca232e5f51e9018889b25038c219552e05
Jezer Mejía <amyuki4@icloud.com>
2024-05-14 02:51:56 -0600

    Handle ConstraintViolations

--- server/src/main/java/ni/factorizacion/server/RestExceptionHandler.java
+++ server/src/main/java/ni/factorizacion/server/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package ni.factorizacion.server;
 
+import jakarta.validation.ConstraintViolationException;
 import ni.factorizacion.server.dtos.GeneralResponse;
 import ni.factorizacion.server.types.ControlException;
 import ni.factorizacion.server.utils.ErrorTools;
@@ -20,12 +21,19 @@ public class RestExceptionHandler {
         return GeneralResponse.getResponse(ex.getStatus(), ex.getMessage(), null);
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<GeneralResponse<Object>> handleViolation(ConstraintViolationException ex) {
+        var errors = ex.getConstraintViolations();
+        var errorsMap = ErrorTools.mapConstraintErrors(errors);
+        return GeneralResponse.getResponse(HttpStatus.BAD_REQUEST, "Field errors", errorsMap);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<GeneralResponse<Map<String, List<String>>>> handleMethodArgumentsNotValid(MethodArgumentNotValidException ex) {
         var bindingResult = ex.getBindingResult();
         var errors = bindingResult.getFieldErrors();
 
-        var errorsMap = ErrorTools.mapErrors(errors);
+        var errorsMap = ErrorTools.mapFieldErrors(errors);
 
         return GeneralResponse.getResponse(HttpStatus.BAD_REQUEST, "Field errors", errorsMap);
     }
--- server/src/main/java/ni/factorizacion/server/utils/ErrorTools.java
+++ server/src/main/java/ni/factorizacion/server/utils/ErrorTools.java
@@ -1,20 +1,29 @@
 package ni.factorizacion.server.utils;
 
+import jakarta.validation.ConstraintViolation;
 import org.springframework.validation.FieldError;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ErrorTools {
-    static public Map<String, List<String>> mapErrors(List<FieldError> errors) {
+    static public Map<String, List<String>> mapFieldErrors(List<FieldError> errors) {
         Map<String, List<String>> errorMap = new HashMap<>();
 
         errors.forEach(error -> {
             var _errors = errorMap.computeIfAbsent(error.getField(), key -> new ArrayList<>());
             _errors.add(error.getDefaultMessage());
             errorMap.put(error.getField(), _errors);
+        });
+
+        return errorMap;
+    }
+    static public Map<String, List<String>> mapConstraintErrors(Set<ConstraintViolation<?>> exceptions) {
+        Map<String, List<String>> errorMap = new HashMap<>();
+
+        exceptions.forEach(error -> {
+            var _errors = errorMap.computeIfAbsent(error.getPropertyPath().toString(), key -> new ArrayList<>());
+            _errors.add(error.getMessage());
+            errorMap.put(error.getPropertyPath().toString(), _errors);
         });
 
         return errorMap;
