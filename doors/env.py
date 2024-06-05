baf87f485eccc93755ab8ed6b579c830ff19943d
Jezer Mejía <00543224@uca.edu.sv>
2024-06-05 17:14:50 -0600

    Update code

---
+++ doors/env.py
@@ -0,0 +1,8 @@
+SSID = "iPhone de Jezer"
+PASSWORD = "123456789"
+SERVER_ADDRESS = "192.168.1.13"
+SERVER_PORT = 8080
+
+# Terminal UUID
+# TODO: Maybe try to get this UUID from the server?
+TERMINAL = b"eb85ec1b-98ca-43d5-a6d0-605c82ed37a9"
--- doors/main.py
+++ doors/main.py
@@ -1,31 +1,18 @@
 from machine import Pin, PWM
 import socket
 import network
-import time
 import uasyncio as asyncio
+from led import led
+from servo import open_gate
 
-SSID = "CLARO1_81DEA0"
-PASSWORD = "G61S8ONKBC"
-SERVER_ADDRESS = "192.168.1.13"
-SERVER_PORT = 8080
+import env
+from wifi import wifi_init
 
-# Terminal UUID
-# TODO: Maybe try to get this UUID from the server?
-TERMINAL = b"eb85ec1b-98ca-43d5-a6d0-605c82ed37a9"
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(SSID, PASSWORD)
-
-while not wlan.isconnected() and wlan.status() >= 0:
-    print("Waiting to connect:")
-    time.sleep(1)
-
-print(wlan.ifconfig())
+wifi_init(env.SSID, env.PASSWORD)
 
 # Socket management
 
-ai = socket.getaddrinfo(SERVER_ADDRESS, SERVER_PORT)
+ai = socket.getaddrinfo(env.SERVER_ADDRESS, env.SERVER_PORT)
 addr = ai[0][-1]
 
 s = socket.socket()
@@ -33,20 +20,6 @@ s.connect(addr)
 s.send(b"GET /sse/terminal/entry HTTP/1.0\r\n\r\n")
 
 s.setblocking(False)
-
-servo = PWM(Pin(0))
-servo.freq(50)
-servo.duty_u16(1310) # Set servo at initial point
-
-async def open_gate(duration: int):
-    global servo
-    led = Pin("LED", Pin.OUT)
-
-    led.value(1)
-    servo.duty_u16(4510) # Set servo at 90°
-    await asyncio.sleep_ms(duration)
-    led.value(0)
-    servo.duty_u16(1310) # Set servo at initial point
 
 def parse_event(event: bytes):
     split = event.split(b"\n")
@@ -85,7 +58,7 @@ async def main():
             print(f"Event: {event}")
             print()
 
-            if (event == b"entry-added" and data == TERMINAL):
+            if (event == b"entry-added" and data == env.TERMINAL):
                 await open_gate(2000)
 
 asyncio.run(main())
