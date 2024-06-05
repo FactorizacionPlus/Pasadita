7c22321946cda6e004e3d7c255ae7194b5e4d1d0
LeoCorea <00542724@uca.edu.sv>
2024-06-05 00:54:32 -0600

    Added a search bar in TerminalView to tested

--- client/src/views/admin-panel/TerminalView.vue
+++ client/src/views/admin-panel/TerminalView.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import AdminTerminal from "../../components/Administrador/AdminTerminal.vue"
+import SearchBar from "../../components/SearchBar.vue"
 import type Terminal from "../../types/AdminTerminal"
-const terminalList : Terminal[] = [
+const terminalList: Terminal[] = [
   {
-  type: "Peatonal",
-  numentries: 10
+    type: "Peatonal",
+    numentries: 10
   },
   {
-  type: "Vehicular",
-  numentries: 11
+    type: "Vehicular",
+    numentries: 11
   },
   {
-  type: "Vigilante",
-  numentries: 12
+    type: "Vigilante",
+    numentries: 12
   },
-  
+
 ]
 </script>
 
 <template>
-      <div class="grid grid-cols-5 gap-5 p-8">
+  <div class="px-8">
+    <SearchBar/>
+  </div>
+  <div class="grid grid-cols-5 gap-5 p-8">
     <div v-for="terminal in terminalList" :key="terminal.type">
       <AdminTerminal :terminal="terminal" />
     </div>
