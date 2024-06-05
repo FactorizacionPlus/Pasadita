72cb8b30e0ec985588fd28917e5abdf301ff5578
LeoCorea <00542724@uca.edu.sv>
2024-06-04 22:31:39 -0600

    Added a new admin panel view

---
+++ client/src/views/admin-panel/TerminalView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import AdminTerminal from "../../components/Administrador/AdminTerminal.vue"
+import type Terminal from "../../types/AdminTerminal"
+const terminalList : Terminal[] = [
+  {
+  type: "Peatonal",
+  numentries: 10
+  },
+  {
+  type: "Vehicular",
+  numentries: 11
+  },
+  {
+  type: "Vigilante",
+  numentries: 12
+  },
+  
+]
+</script>
+
+<template>
+      <div class="grid grid-cols-5 gap-5 p-8">
+    <div v-for="terminal in terminalList" :key="terminal.type">
+      <AdminTerminal :terminal="terminal" />
+    </div>
+  </div>
+</template>
