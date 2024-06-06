d7324a35620b15db27843b8cdbebe48d6a32c8ea
Poncka <davidponce600@gmail.com>
2024-06-05 18:06:17 -0600

    AdminNavbarMenu

--- client/src/layouts/AdminLayout.vue
+++ client/src/layouts/AdminLayout.vue
@@ -1,16 +1,16 @@
 <template>
-    <div>
-      <header>
-        <h2>Admin</h2>
-      </header>
-      <main>
-        <router-view/>
-      </main>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'AdminLayout',
-  };
-  </script>
+  <div class="flex h-[100vh]">
+    <AdminNavigationMenu class="flex-shrink-0"/>
+    <main class="flex-shrink-0 flex-grow-[1] p-4 overflow-y-auto">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AdminNavigationMenu from '../components/AdminNavigationMenu.vue';
+</script>
+
+<style scoped>
+
+</style>
