39292771f153c72dae4ef2d024273079730b0b86
LeoCorea <00542724@uca.edu.sv>
2024-06-08 01:15:42 -0600

    change div to li

--- client/src/components/Cards/TerminalCard.vue
+++ client/src/components/Cards/TerminalCard.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="flex flex-col max-w-sm bg-white">
+    <li class="flex flex-col bg-white">
         <!--Information-->
         <div class="flex items-center justify-center border-t border-l border-r border-pasadita-shade-2 px-2 py-2 rounded-t-lg gap-2">
             <div class="flex items-center ">
@@ -39,6 +39,5 @@ const props = defineProps<{
                 <VueFeather type="eye" stroke-width="3.5" size="16"></VueFeather>
             </button>
         </div>
-    </div>
-
+    </li>
 </template>
