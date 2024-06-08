fd96e7080b90ae92f7fb1c0a0270a4110bcd35ce
LeoCorea <00542724@uca.edu.sv>
2024-06-08 01:15:01 -0600

    Added new Admin panel views

---
+++ client/src/views/admin-panel/ResidenteView.vue
@@ -0,0 +1,7 @@
+<script  setup lang="ts">
+    import CurrentPageInfo from "@/components/CurrentPageInfo.vue"
+</script>
+
+<template>
+    <CurrentPageInfo title="Residentes" icon="user"/>
+</template>
