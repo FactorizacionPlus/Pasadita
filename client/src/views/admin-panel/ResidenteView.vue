3062c7f704b53ecdda006b80c79bffadff023e01
LeoCorea <00542724@uca.edu.sv>
2024-06-08 21:50:25 -0600

    Deleted burnet data from admin-panel views

--- client/src/views/admin-panel/ResidenteView.vue
+++ client/src/views/admin-panel/ResidenteView.vue
@@ -1,7 +1,29 @@
-<script  setup lang="ts">
-    import CurrentPageInfo from "@/components/CurrentPageInfo.vue"
+<script setup lang="ts">
+import CurrentPageInfo from "@/components/CurrentPageInfo.vue"
+import ResidentCard from "@/components/Cards/ResidentCard.vue"
+import type Resident from "@/types/InvitedResident";
+
+const residentList: Resident[] = [
+  {
+    name: "Pedro",
+    secondName: "Zelaya",
+    identification: "C3049392",
+    entries: 0,
+    image: ""
+  }]
+
 </script>
 
 <template>
-    <CurrentPageInfo title="Residentes" icon="user"/>
+
+  <div class="flex flex-col gap-10">
+    <div>
+      <CurrentPageInfo title="Residentes" icon="user" />
+      <ResidentCard :resident="resident" v-for="resident in residentList" :key="resident.identification" />
+    </div>
+    <div>
+      <CurrentPageInfo title="Residentes" icon="user" />
+      <ResidentCard :resident="resident" v-for="resident in residentList" :key="resident.identification" />
+    </div>
+  </div>
 </template>
