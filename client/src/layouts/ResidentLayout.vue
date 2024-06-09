42a4e172827b6380bbf5b02a0576328b7cfa1aee
Mrsty21 <riverasoto.marcelo@gmail.com>
2024-06-09 17:01:28 -0600

    resident view made

--- client/src/layouts/ResidentLayout.vue
+++ client/src/layouts/ResidentLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col h-screen mx-auto">
+  <div class="flex flex-col mx-auto">
     <DefaultNavigationMenu />
     <main class="flex-shrink-0 w-full max-w-screen-2xl mx-auto flex-grow-[1] p-4 overflow-y-auto">
       <router-view />
--- client/src/views/resident-panel/HomeView.vue
+++ client/src/views/resident-panel/HomeView.vue
@@ -2,6 +2,8 @@
 import CardsContainer from "@/components/CardsContainer.vue"
 import ResidentCard from "@/components/Cards/ResidentCard.vue"
 import VueFeather from 'vue-feather';
+import PaginationItem from "@/components/PaginationItem.vue"
+import SearchBar from "@/components/SearchBar.vue"
 
 const residet1 = {
   name: "marcelo",
@@ -12,9 +14,9 @@ const residet1 = {
 </script>
 
 <template>
-  <div class="flex flex-col w-full items-center">
+  <div class="flex flex-col w-full items-center mb-5">
     <button
-      class="bg-pasadita-blue-4 text-pasadita-blue-2 text-2xl rounded-lg border border-pasadita-shade-2 py-7 w-[20rem] flex flex-col items-center">
+      class="bg-pasadita-blue-4 text-pasadita-blue-2 text-2xl rounded-lg border border-pasadita-shade-2 hover:bg-pasadita-blue-2/20 hover:rounded-2xl active:scale-95 transition-all py-7 w-[20rem] flex flex-col items-center">
       <div class="flex justify-center items-center flex-shrink-0">
         <VueFeather type="edit" size="40" stroke-width="2"></VueFeather>
       </div>
@@ -22,8 +24,8 @@ const residet1 = {
     </button>
   </div>
   <CardsContainer title="Solicitudes" icon="grid">
-    <div class="grid grid-cols-4 gap-4">
-
+    <SearchBar />
+    <div class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-5">
       <ResidentCard :resident="residet1" />
       <ResidentCard :resident="residet1" />
       <ResidentCard :resident="residet1" />
@@ -32,4 +34,5 @@ const residet1 = {
       <ResidentCard :resident="residet1" />
     </div>
   </CardsContainer>
+  <PaginationItem :totalPages=5 />
 </template>
