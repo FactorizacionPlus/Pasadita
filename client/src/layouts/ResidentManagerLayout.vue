debb28fe7669855afd9f31d235a75f040229fcab
Poncka <davidponce600@gmail.com>
2024-06-09 15:21:19 -0600

    Historial de solicitudes

--- client/src/layouts/ResidentManagerLayout.vue
+++ client/src/layouts/ResidentManagerLayout.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <header>
-      <h2>Residente General</h2>
-    </header>
-    <main>
+  <div class="flex flex-col h-screen mx-auto">
+    <DefaultNavigationMenu />
+    <main class="flex-shrink-0 w-full max-w-screen-2xl mx-auto flex-grow-[1] p-4 overflow-y-auto">
       <router-view />
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import DefaultNavigationMenu from '@/components/NavigationMenu/DefaultNavigationMenu.vue';
+
 </script>
+
+<style scoped></style>
