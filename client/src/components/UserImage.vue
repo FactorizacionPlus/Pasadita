9569b1ce2ad5b451e2442a20266c24ed1bc02a9f
LeoCorea <00542724@uca.edu.sv>
2024-06-09 01:53:14 -0600

    Added a Image component

---
+++ client/src/components/UserImage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    import VueFeather from 'vue-feather';
+    const props = defineProps<{ image: string, size: string, padding: string }>();
+</script>
+
+<template>
+    <img v-if="props.image" :src="props.image" alt="User Image"
+        class="w-full h-full object-cover rounded-full" />
+    <VueFeather v-else type="user" :size="props.size" stroke="#004f82" stroke-width="1.5" :style="{ padding: props.padding }" class="!flex"></VueFeather>
+</template>
