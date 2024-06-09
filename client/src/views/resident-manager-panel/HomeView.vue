8bf30681d49b966fc939546c6944113985014c02
Poncka <davidponce600@gmail.com>
2024-06-09 17:35:05 -0600

    residente encargado

--- client/src/views/resident-manager-panel/HomeView.vue
+++ client/src/views/resident-manager-panel/HomeView.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
 import VueFeather from 'vue-feather';
 
+const ROUTE = "/residente-encargado/";
+const routes = [
+    {
+        text: "Gestionar permisos",
+        link: "permisos",
+        icon: "file-minus"
+    },
+    {
+        text: "Gestionar residentes",
+        link: "residentes",
+        icon: "users"
+    }
+]
 </script>
 
 <template>
-    <div>
+    <div class="grid grid-cols-[repeat(2,0.3fr)] justify-center items-center h-[100%] gap-8">
+                <RouterLink :key="index" v-for="route, index in routes"
+                    class="flex flex-col min-w-32 gap-2  justify-center items-center h-96 font-semibold rounded-md text-2xl bg-pasadita-blue-4 text-pasadita-blue-2 hover:bg-pasadita-blue-2 hover:text-pasadita-blue-4 transition-all active:scale-95"
+                    :to="`${ROUTE}${route.link}`">
 
+                    <div class="ml-1 flex justify-center items-center flex-shrink-0 ">
+                        <VueFeather :type="route.icon" size="150" stroke-width="1.5"></VueFeather>
+                    </div>
+                    
+                    {{ route.text }}
+                </RouterLink>
     </div>
 </template>
 
