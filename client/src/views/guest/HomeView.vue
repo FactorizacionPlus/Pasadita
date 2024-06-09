f110ec64b23c3b30e25c28755667e3869a582688
LeoCorea <00542724@uca.edu.sv>
2024-06-09 04:14:20 -0600

    Added a History view

---
+++ client/src/views/guest/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import VueFeather from "vue-feather";
+</script>
+
+<template>
+<div class="flex h-screen items-center justify-center">
+    <button type="button" class="inline-flex items-center rounded-lg bg-pasadita-blue-4 p-2.5 text-center text-sm font-semibold text-pasadita-blue-2 transition-all hover:rounded-xl hover:bg-pasadita-blue-2 hover:text-white active:scale-95">
+        <div class="flex flex-col items-center justify-center gap-2 p-5">
+            <VueFeather type="clock" size="125" stroke-width="1.5"/>
+            <span class="text-xl">Ver Permiso Activo</span>
+        </div>
+    </button>
+</div>
+</template>
