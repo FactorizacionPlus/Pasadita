92fd6dc49b66b9d9cb83d321e9f9657a271a33cc
LeoCorea <00542724@uca.edu.sv>
2024-06-10 06:29:56 -0600

    Configure and create Login desing

--- client/src/views/guest/HomeView.vue
+++ client/src/views/guest/HomeView.vue
@@ -2,23 +2,22 @@
 import { ref } from "vue";
 import VueFeather from "vue-feather";
 import Modal from "@/components/Modal/Guest/PermissionActive.vue";
-import IconComponent from "@/assets/Passaport.svg?component";
+import ModalNoActive from "@/components/Modal/Guest/NoActivePermission.vue"
 const modal = ref<typeof Modal>();
+const modalNoActive = ref<typeof Modal>();
 </script>
 
 <template>
   <div class="flex h-screen items-center justify-center">
-    <button
-      type="button"
+    <button type="button"
       class="inline-flex items-center rounded-lg bg-pasadita-blue-4 p-2.5 text-center text-sm font-semibold text-pasadita-blue-2 transition-all hover:rounded-xl hover:bg-pasadita-blue-2 hover:text-white active:scale-95"
-      @click="modal?.show()"
-    >
+      @click="modalNoActive?.show()">
       <div class="flex flex-col items-center justify-center gap-2 p-5">
         <VueFeather type="clock" size="125" stroke-width="1.5" />
         <span class="text-xl">Ver Permiso Activo</span>
       </div>
     </button>
-    <IconComponent class="text-pasadita-blue-2 hover:text-red-950" />
   </div>
-  <Modal ref="modal"> Hola </Modal>
+  <Modal ref="modal">Hola</Modal>
+  <ModalNoActive ref="modalNoActive">Hola</ModalNoActive>
 </template>
---
+++ client/src/views/guest/LoginView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Pasadita from "@/assets/PasaditaLogoV2.svg?url";
+import Google from "@/assets/Google.svg?url"
+
+</script>
+
+<template>
+    <div class="flex  h-screen flex-col items-center justify-center gap-5">
+        <img :src="Pasadita" class="size-72" />
+        <div>
+            <button
+                class="inline-flex items-center gap-2 rounded-md border border-pasadita-shade-2 bg-white px-4 py-2 text-pasadita-shade-1 transition-all hover:rounded-xl hover:bg-pasadita-blue-4 active:scale-95">
+                <img :src="Google" class="size-6" />
+                <span>Continuar con Google</span>
+            </button>
+        </div>
+    </div>
+</template>
