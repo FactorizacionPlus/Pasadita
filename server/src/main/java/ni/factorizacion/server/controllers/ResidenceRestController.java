c2350001d648b3b5d3f0e5e5f1693b77f234e2de
David Parrales <105189666+Poncka@users.noreply.github.com>
2024-06-30 10:44:20 -0600

    add resident to residence (#87)
    * ResidentToResidence
    
    * Validation of the number of permitted inhabitants
    
    * Funcionalidad cambiada en el RestController
    
    * cambios menores

--- server/src/main/java/ni/factorizacion/server/controllers/ResidenceRestController.java
+++ server/src/main/java/ni/factorizacion/server/controllers/ResidenceRestController.java
@@ -2,21 +2,32 @@ package ni.factorizacion.server.controllers;
 
 import jakarta.validation.Valid;
 import ni.factorizacion.server.domain.dtos.GeneralResponse;
+import ni.factorizacion.server.domain.dtos.input.AssignResidentDto;
 import ni.factorizacion.server.domain.dtos.output.ResidenceSimpleDto;
 import ni.factorizacion.server.domain.dtos.input.SaveResidenceDto;
 import ni.factorizacion.server.domain.entities.Residence;
+import ni.factorizacion.server.domain.entities.Resident;
 import ni.factorizacion.server.services.ResidenceService;
+import ni.factorizacion.server.services.ResidentService;
+import ni.factorizacion.server.types.ControlException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/residence")
 public class ResidenceRestController {
+
     @Autowired
     private ResidenceService service;
+
+    @Autowired
+    private ResidentService residentService;
 
     @GetMapping
     public ResponseEntity<GeneralResponse<List<ResidenceSimpleDto>>> getAllResidences() {
@@ -34,13 +45,32 @@ public class ResidenceRestController {
         return GeneralResponse.ok("Residence saved", null);
     }
 
-    @DeleteMapping(path = "/{uuid}")
+    @DeleteMapping(value = "/{uuid}")
     public void removeResidence(@PathVariable("uuid") String uuid) throws Exception {
         service.removeResidence(uuid);
     }
 
-    @PutMapping(path = "/{uuid}")
+    @PutMapping(value = "/{uuid}")
     public void updateUser(@PathVariable("uuid") String uuid, @RequestBody @Valid SaveResidenceDto dto) throws Exception {
         service.updateResidence(uuid, dto);
+    }
+
+    @PostMapping(consumes = "application/json", value = "/assign")
+    public ResponseEntity<GeneralResponse<Residence>> assignResidenteToResidencia(@Valid @RequestBody AssignResidentDto request) throws ControlException {
+        Optional<Resident> optionalResidente = residentService.findByIdentifier(request.getIdentifier());
+        if (optionalResidente.isEmpty()) {
+            return GeneralResponse.error404("Resident does not exist");
+        }
+
+        Optional<Residence> optionalResidencia = service.findById(UUID.fromString(request.getUuid()));
+        if (optionalResidencia.isEmpty()) {
+            return GeneralResponse.error404("Residence does not exist");
+        }
+
+        Resident residente = optionalResidente.get();
+        Residence residencia = optionalResidencia.get();
+
+        service.assignResidenteToResidencia(residente, residencia);
+        return GeneralResponse.ok("Resident assigned to residence", null);
     }
 }
--- server/src/main/java/ni/factorizacion/server/services/ResidenceService.java
+++ server/src/main/java/ni/factorizacion/server/services/ResidenceService.java
@@ -1,11 +1,14 @@
 package ni.factorizacion.server.services;
 
+import ni.factorizacion.server.domain.dtos.input.AssignResidentDto;
 import ni.factorizacion.server.domain.dtos.input.SaveResidenceDto;
 import ni.factorizacion.server.domain.entities.Residence;
+import ni.factorizacion.server.domain.entities.Resident;
 import ni.factorizacion.server.types.ControlException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ResidenceService {
 
@@ -13,9 +16,13 @@ public interface ResidenceService {
 
     Optional<Residence> findByDescription(String description);
 
+    Optional<Residence> findById(UUID uuid);
+
     void saveResidence(SaveResidenceDto dto) throws ControlException;
 
     void updateResidence(String uuid, SaveResidenceDto dto) throws ControlException;
 
     void removeResidence(String uuid) throws ControlException;
+
+    void assignResidenteToResidencia(Resident resident, Residence residence) throws ControlException;
 }
