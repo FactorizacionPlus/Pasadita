8993dc4dd5a2ccf73090952850bb7f70c964318f
Jezer Mejía <00543224@uca.edu.sv>
2024-07-04 22:33:23 -0600

    Add route blocking depending on user role/terminal

--- client/src/router/auth.ts
+++ client/src/router/auth.ts
@@ -1,6 +1,8 @@
 import { useAuth } from "@/stores/auth";
 import { useTerminal } from "@/stores/terminal";
+import { useUser } from "@/stores/user";
 import type { RouteRecordName, Router } from "vue-router";
+import { RoleRootPath, type CustomRouteMeta, type ViewRole } from ".";
 
 const allowedAnonymous: RouteRecordName[] = [
   "login",
@@ -11,39 +13,57 @@ const allowedAnonymous: RouteRecordName[] = [
 
 export function applyAuthRouting(router: Router) {
   const auth = useAuth();
+  const user = useUser();
   const terminal = useTerminal();
 
   router.beforeEach((to, from, next) => {
     console.log({ to, from });
-    const isLoggedIn = auth.token != null || terminal.terminalLogin != null;
 
     const isInAnonymous = allowedAnonymous.includes(to.name ?? "");
 
-    if (isLoggedIn) {
-      // Logged in
+    let role: ViewRole | null = null;
+    if (user.user) {
+      role = user.user.role;
+    } else if (terminal.terminalLogin) {
+      if (terminal.terminalLogin.type == "MANUAL") {
+        role = "TERMINAL_MANUAL";
+      } else {
+        role = "TERMINAL";
+      }
+    }
 
+    if (role != null) {
+      // Logged in
       if (to.path.startsWith("/auth/logout")) {
         auth.logout();
         terminal.logout();
         return next("/auth/login");
       }
 
+      const rootPath = RoleRootPath[role];
+
       if (isInAnonymous) {
         // Is accessing anonymous routes like "login" and "register"
-        return next("/");
-      } else {
-        // Allow if logged in
+        return next(rootPath);
+      }
+
+      const meta = to.meta as CustomRouteMeta;
+      if (role == meta.role) {
         return next();
       }
+
+      // Allow if logged in
+      if (to.path != rootPath) return next(rootPath);
+      else return next();
     } else {
       // Not logged in
       if (isInAnonymous) {
         // Allow anonymous routes
         return next();
-      } else {
-        // Deny other routes
-        return next("/auth/login");
       }
+
+      // Deny other routes
+      return next("/auth/login");
     }
   });
 }
