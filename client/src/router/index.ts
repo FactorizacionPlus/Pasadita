bff2d806069ea506f5f8b143caca1193930de117
Jezer Mejía <92834478+JezerMejia@users.noreply.github.com>
2024-07-04 22:34:39 -0600

    Add route blocking depending on user role/terminal (#100)

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,7 +1,24 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, type RouteMeta } from "vue-router";
 import GuestLayout from "@/layouts/GuestLayout.vue";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import AdminLayout from "@/layouts/AdminLayout.vue";
+import type { UserRole } from "@/types/User/UserRole";
+
+export type ViewRole = UserRole | "TERMINAL" | "TERMINAL_MANUAL";
+
+export const RoleRootPath: { [key in ViewRole]: string } = {
+  ROLE_INVITED: "/invitado",
+  ROLE_RESIDENT: "/residente",
+  ROLE_RESIDENT_SUDO: "/residente-encargado",
+  ROLE_ADMIN: "/admin",
+  TERMINAL: "/terminal/camera",
+  TERMINAL_MANUAL: "/terminal",
+};
+
+export interface CustomRouteMeta extends RouteMeta {
+  rootPath?: string;
+  role: ViewRole;
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,11 +30,6 @@ const router = createRouter({
         {
           path: "/",
           name: "page-home",
-          component: () => import("../views/HomeView.vue"),
-        },
-        {
-          path: "login",
-          name: "login",
           component: () => import("../views/HomeView.vue"),
         },
       ],
@@ -52,6 +64,7 @@ const router = createRouter({
       path: "/admin",
       name: "admin",
       component: AdminLayout,
+      meta: { rootPath: "/admin", role: "ROLE_ADMIN" } as CustomRouteMeta,
       children: [
         {
           path: "",
@@ -99,13 +112,8 @@ const router = createRouter({
       path: "/invitado",
       name: "guest",
       component: DefaultLayout,
-      meta: { rootPath: "/invitado", roleToShow: "Invitado" },
+      meta: { rootPath: "/invitado", role: "ROLE_INVITED" } as CustomRouteMeta,
       children: [
-        {
-          path: "login",
-          name: "guest-login",
-          component: () => import("../views/guest/LoginView.vue"),
-        },
         {
           path: "",
           name: "guest-home",
@@ -117,7 +125,7 @@ const router = createRouter({
       path: "/residente",
       name: "resident",
       component: DefaultLayout,
-      meta: { rootPath: "/residente", roleToShow: "Residente" },
+      meta: { rootPath: "/residente", role: "ROLE_RESIDENT" } as CustomRouteMeta,
       children: [
         {
           path: "",
@@ -135,7 +143,7 @@ const router = createRouter({
       path: "/residente-encargado",
       name: "resident-chief",
       component: DefaultLayout,
-      meta: { rootPath: "/residente-encargado", roleToShow: "Residente Encargado" },
+      meta: { rootPath: "/residente-encargado", role: "ROLE_RESIDENT_SUDO" } as CustomRouteMeta,
       children: [
         {
           path: "",
@@ -158,15 +166,18 @@ const router = createRouter({
       path: "/terminal",
       name: "terminal",
       component: DefaultLayout,
+      meta: { rootPath: "/terminal", role: "TERMINAL" } as CustomRouteMeta,
       children: [
         {
           path: "",
           name: "terminal-home",
+          meta: { role: "TERMINAL_MANUAL" } as CustomRouteMeta,
           component: () => import("../views/terminal/HomeView.vue"),
         },
         {
           path: "camera",
           name: "terminal-camera",
+          meta: { role: "TERMINAL" } as CustomRouteMeta,
           component: () => import("../views/terminal/CameraView.vue"),
         },
       ],
