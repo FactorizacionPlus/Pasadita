e2d43c7e06945cfc90b330a7e0882fbb2b3066b5
Poncka <davidponce600@gmail.com>
2024-07-06 11:17:08 -0600

    Listar terminales

---
+++ server/src/main/java/ni/factorizacion/server/domain/dtos/output/TerminalSimpleDto.java
@@ -0,0 +1,25 @@
+package ni.factorizacion.server.domain.dtos.output;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import ni.factorizacion.server.domain.entities.Terminal;
+
+import java.util.UUID;
+
+@Data
+@SuperBuilder
+public class TerminalSimpleDto {
+    private UUID uuid;
+    private String type;
+
+    static public TerminalSimpleDto from(Terminal terminal) {
+        return TerminalSimpleDto.builder()
+                .uuid(terminal.getUuid())
+                .type(terminal.getType().toString())
+                .build();
+    }
+}
+
+
+
+
