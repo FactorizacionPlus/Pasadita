835536724aeaa595cc052e247b3826f7bcdb2449
David Quintanilla Ruiz <93105948+davidquintr@users.noreply.github.com>
2024-07-06 01:50:11 -0600

    the endpoint for finish user registration (#102)

--- server/src/main/java/ni/factorizacion/server/controllers/RegisteredUserRestController.java
+++ server/src/main/java/ni/factorizacion/server/controllers/RegisteredUserRestController.java
@@ -1,6 +1,7 @@
 package ni.factorizacion.server.controllers;
 
 import ni.factorizacion.server.domain.dtos.GeneralResponse;
+import ni.factorizacion.server.domain.dtos.input.FinishRegisterDto;
 import ni.factorizacion.server.domain.entities.InvitedUser;
 import ni.factorizacion.server.domain.entities.RegisteredUser;
 import ni.factorizacion.server.domain.entities.Resident;
@@ -9,6 +10,7 @@ import ni.factorizacion.server.services.RegisteredUserService;
 import ni.factorizacion.server.services.ResidentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,5 +64,23 @@ public class RegisteredUserRestController {
         residentService.save(resident.get());
 
         return GeneralResponse.ok("Migrated to resident", null);
+    }
+
+    @PreAuthorize("hasRole('ROLE_INVITED')")
+    @PostMapping("/finish-register")
+    public ResponseEntity<GeneralResponse<String>> finishRegister(@RequestBody FinishRegisterDto finishRegisterDto) {
+        Optional<RegisteredUser> registeredUser = registeredUserService.findByEmail(finishRegisterDto.getEmail());
+
+        if (registeredUser.isEmpty()) {
+            return GeneralResponse.error404("User not found.");
+        }
+
+        registeredUser.get().setIdentifier(finishRegisterDto.getIdentifier());
+        registeredUser.get().setIdentifierType(finishRegisterDto.getIdentifierType());
+
+        Optional<InvitedUser> invitedUser = invitedUserService.createFrom(registeredUser.get());
+
+        invitedUserService.save(invitedUser.orElse(null));
+        return GeneralResponse.ok("Finished Register", null);
     }
 }
