32e488482ecbce61d6d5f892ba353aea4f08a92d
Jezer Mejía <00543224@uca.edu.sv>
2024-07-06 11:59:04 -0600

    List all Residents per Residence

--- server/src/main/java/ni/factorizacion/server/repositories/ResidentRepository.java
+++ server/src/main/java/ni/factorizacion/server/repositories/ResidentRepository.java
@@ -1,11 +1,15 @@
 package ni.factorizacion.server.repositories;
 
+import ni.factorizacion.server.domain.entities.Residence;
 import ni.factorizacion.server.domain.entities.Resident;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ResidentRepository extends JpaRepository<Resident, UUID> {
     Optional<Resident> findByIdentifier(String identifier);
+
+    List<Resident> findAllByResidence(Residence residence);
 }
--- server/src/main/java/ni/factorizacion/server/services/ResidentService.java
+++ server/src/main/java/ni/factorizacion/server/services/ResidentService.java
@@ -2,6 +2,7 @@ package ni.factorizacion.server.services;
 
 import ni.factorizacion.server.domain.dtos.input.SaveResidentDto;
 import ni.factorizacion.server.domain.entities.RegisteredUser;
+import ni.factorizacion.server.domain.entities.Residence;
 import ni.factorizacion.server.domain.entities.Resident;
 import ni.factorizacion.server.domain.entities.ResidentRole;
 import ni.factorizacion.server.types.ControlException;
@@ -12,6 +13,8 @@ import java.util.Optional;
 
 public interface ResidentService {
     List<Resident> findAll();
+
+    List<Resident> findAllByResidence(Residence residence);
 
     Optional<Resident> findByIdentifier(String identifier);
 
