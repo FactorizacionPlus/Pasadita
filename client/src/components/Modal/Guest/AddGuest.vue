8ca25b2e269baf575419cbe3cbacb701d7e24e64
LeoCorea <00542724@uca.edu.sv>
2024-07-06 11:52:18 -0600

    Add POST configuration in Add Guest

--- client/src/components/Modal/Guest/AddGuest.vue
+++ client/src/components/Modal/Guest/AddGuest.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, defineEmits, type Ref } from "vue";
 import HeaderModal from "@/components/Modal/HeaderModal.vue";
 import VueFeather from "vue-feather";
 import Modal from "@/components/Modal/ModalComponent.vue";
 import UserImage from "@/components/UserImage.vue";
 import InputForm from "@/components/Forms/InputForm.vue";
-
 import BodyModal from "@/components/Modal/BodyModal.vue";
 import ControlsModal from "../ControlsModal.vue";
 import IdentityTypeSelection from "../IdentityTypeSelection.vue";
@@ -14,15 +13,34 @@ import type Alert from "@/types/Alert";
 import { AlertType } from "@/types/Alert";
 import checkIsValidIdentifier from "@/utils/checkIsValidIdentifier";
 import type { IdentifierType } from "@/types/User/IdentifierType";
+import { useUser } from "@/stores/user";
+import type RegisteredUser from "@/types/User/RegisteredUser";
+import type SaveIdentification from "@/types/Guest/SaveIdentification";
+import { requestIdentification } from "@/composables/requestIdentification";
+import type { ErrorMap } from "@/types/ErrorMap";
+import { setValidationErrorForm, type InputFormType } from "@/utils/formValidation";
 
 const modal = ref<typeof Modal>();
-
 const alertForIdentifier = ref<Alert | undefined>();
+const user = useUser();
 
 const data = ref<{
+  registeredUser: RegisteredUser,
   identifierType: IdentifierType,
   identifier: string,
-}>({ identifierType: "DUI", identifier: "" });
+}>({registeredUser: user.user as RegisteredUser , identifierType: "DUI", identifier: "" });
+
+const emit = defineEmits(['toggle-menu']);
+
+const formData = ref<SaveIdentification>({
+  indentifier:"",
+  email:data.value.registeredUser.email,
+  indentifierType: data.value.identifierType,
+});
+
+
+console.log(data.value.registeredUser)
+console.log(data.value.identifierType)
 
 enum Message {
   BUTTON_ACCEPT = "Aceptar",
@@ -35,8 +53,12 @@ defineExpose({
   close: () => modal.value?.close(),
 });
 
+const identificationInput = ref<InputFormType>();
+const inputMap = new Map<string, Ref<InputFormType | undefined>>();
+
 onMounted(() => {
   modal.value?.show();
+  inputMap.set(identificationInput.value!.props.name, identificationInput);
 });
 
 const alertFillData: Alert = {
@@ -44,8 +66,30 @@ const alertFillData: Alert = {
   type: AlertType.INFO,
 };
 
-function handleSubmit(event: Event) {
+async function request(): Promise<boolean> {
+  console.log(formData.value)
+  const { data, statusCode } = await requestIdentification(formData.value);
+
+  if (!data.value) {
+    return false;
+  }
+
+  if (statusCode.value == 400) {
+    const errorMap = data.value?.data as unknown as ErrorMap;
+    setValidationErrorForm(inputMap, errorMap);
+  }
+
+  return data.value?.ok;
+}
+
+async function handleSubmit(event: Event) {
   event.preventDefault();
+
+  const valid = await request();
+
+  if (valid) {
+    formData.value.indentifier = "";
+  }
 
   if (!data.value.identifier) {
     const alert: Alert = {
@@ -66,6 +110,7 @@ function handleSubmit(event: Event) {
   }
   resetValues();
   modal.value?.close();
+  emit('toggle-menu');
 }
 
 function resetValues() {
@@ -78,17 +123,17 @@ function resetValues() {
     <form class="w-full max-w-xl overflow-hidden rounded-md bg-white" @submit="handleSubmit">
       <HeaderModal :title="Message.ACCOUNT" icon="user" action="create" />
       <BodyModal>
-        <UserImage image="" class="size-36 self-center" />
+        <UserImage :image="data.registeredUser.imageUrl" class="size-36 self-center" />
 
         <div class="flex w-full items-center gap-3 text-xl text-pasadita-blue-1">
-          <InputForm name="firstName" title="Nombre" type="text" class="flex-1" />
-          <InputForm name="lastName" title="Apellido" type="text" class="flex-1" />
+          <InputForm :disabled="true" :model-value="data.registeredUser.firstName" name="firstName" title="Nombre" type="text" class="flex-1" />
+          <InputForm :disabled="true" :model-value="data.registeredUser.lastName" name="lastName" title="Apellido" type="text" class="flex-1" />
         </div>
 
         <SimpleAlert :alert="alertFillData" />
         <IdentityTypeSelection :identity-type="data.identifierType" @identity-type="data.identifierType = $event" />
-        <InputForm :modelValue="data.identifier" :alert="alertForIdentifier" name="identifier"
-          @update:value="data.identifier = $event" title="Identificación" type="text" />
+        <InputForm ref="identificationInput" :modelValue="data.identifier" :alert="alertForIdentifier" name="identifier"
+          @update:value="data.identifier = $event" v-model="formData.indentifier" title="Identificación" type="text" />
       </BodyModal>
       <ControlsModal>
         <button type="submit"
