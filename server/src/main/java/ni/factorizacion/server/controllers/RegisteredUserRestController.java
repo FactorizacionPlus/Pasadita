a5837f7d1d47a07422a36c949509833786376fa6
Jezer Mejía <00543224@uca.edu.sv>
2024-07-07 09:30:14 -0600

    Add entryCount field

--- server/src/main/java/ni/factorizacion/server/controllers/RegisteredUserRestController.java
+++ server/src/main/java/ni/factorizacion/server/controllers/RegisteredUserRestController.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Objects;
 import java.util.Optional;
 
 @RestController
