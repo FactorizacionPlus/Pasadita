4319184045c8862fda58a0af35c5a340bc7247d7
LeoCorea <00542724@uca.edu.sv>
2024-07-06 20:37:22 -0600

    Fix stetic details

--- client/src/components/Modal/Guest/NoActivePermission.vue
+++ client/src/components/Modal/Guest/NoActivePermission.vue
@@ -3,7 +3,7 @@ import { ref } from "vue";
 import HeaderModal from "@/components/Modal/HeaderModal.vue";
 import VueFeather from "vue-feather";
 import Modal from "@/components/Modal/ModalComponent.vue";
-import Pasadita from "@/assets/PasaditaLogo.svg?component";
+import Pasadita from "@/assets/PasaditaLogo.svg?url";
 
 const modal = ref<typeof Modal>();
 
@@ -19,20 +19,18 @@ defineExpose({
       <HeaderModal title="Permiso Activo" icon="grid" action="view" />
 
       <div class="flex max-h-[80vh] items-center gap-4 px-4 py-6">
-        <Pasadita />
+
+        <img :src="Pasadita" alt="pasadita">
         <p class="text-3xl font-semibold text-pasadita-blue-1">
-          No Hay Ningun <br />
+          No Hay Ningún <br />
           Permiso Activo
         </p>
       </div>
 
       <!-- Botones -->
       <div class="flex justify-end gap-2 border-t border-t-pasadita-shade-2 p-2">
-        <button
-          @click="modal?.close()"
-          type="button"
-          class="inline-flex items-center rounded-lg bg-pasadita-red-2 p-2 text-center text-sm font-normal text-pasadita-red-0 transition-all hover:rounded-xl hover:bg-pasadita-red-1 active:scale-95"
-        >
+        <button @click="modal?.close()" type="button"
+          class="inline-flex items-center rounded-lg bg-pasadita-red-2 p-2 text-center text-sm font-normal text-pasadita-red-0 transition-all hover:rounded-xl hover:bg-pasadita-red-1 active:scale-95">
           <VueFeather type="x" stroke-width="2.5" size="16"></VueFeather>
           <span>Cancelar</span>
         </button>
