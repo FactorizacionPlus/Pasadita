884134b6d092d1a5360c2433c9e7356d955a93a4
Mrsty21 <riverasoto.marcelo@gmail.com>
2024-07-06 18:08:37 -0600

    fixes1

--- client/src/layouts/DefaultLayout.vue
+++ client/src/layouts/DefaultLayout.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="relative mx-auto flex h-screen flex-col" :class="[contrastClass, fontSizeClass, dyslexicClass]">
+  <div
+    class="relative mx-auto flex h-screen flex-col"
+    :class="[contrastClass, fontSizeClass, dyslexicClass]"
+  >
     <DefaultNavigationMenu :root="root as string" :role="role as string" />
     <main class="mx-auto mt-4 w-full max-w-screen-2xl shrink-0 grow 2xl:mt-8">
       <router-view />
     </main>
-    <ul style="bottom: 8px; right: 8px; max-width: 512px; z-index: 20" class="fixed bottom-4 right-4 w-full max-w-lg">
+    <ul
+      style="bottom: 8px; right: 8px; max-width: 512px; z-index: 20"
+      class="fixed bottom-4 right-4 w-full max-w-lg"
+    >
       <ToastAlert v-for="(toast, index) in toasts" :toast="toast" :key="index" />
     </ul>
   </div>
@@ -25,8 +31,10 @@ const root = route.meta.rootPath;
 
 const preferences = usePreferences();
 
-const contrastClass = computed(() => preferences.preferences.highContrast ? "contrast-150" : "")
-const dyslexicClass = computed(() => preferences.preferences.dyslexicFont ? "!font-openDyslexic" : "")
+const contrastClass = computed(() => (preferences.preferences.highContrast ? "contrast-150" : ""));
+const dyslexicClass = computed(() =>
+  preferences.preferences.dyslexicFont ? "!font-openDyslexic" : ""
+);
 
 const fontSizeClass = computed(() => {
   switch (preferences.preferences.fontSize) {
@@ -37,8 +45,7 @@ const fontSizeClass = computed(() => {
     default:
       return "";
   }
-})
-
+});
 </script>
 
 <style scoped></style>
