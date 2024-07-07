22ef07861d2b6dc950c33434ee385a3212dcc347
Mrsty21 <riverasoto.marcelo@gmail.com>
2024-07-07 00:29:47 -0600

    feature/listEntriesInAdminPanel

--- client/src/components/NavigationMenu/DefaultNavigationMenu.vue
+++ client/src/components/NavigationMenu/DefaultNavigationMenu.vue
@@ -1,6 +1,7 @@
 <template>
   <header
-    class="sticky top-0 z-10 mx-auto flex w-full max-w-screen-2xl items-center justify-between bg-white px-4 py-2 shadow-header 2xl:top-4 2xl:rounded-xl">
+    class="sticky top-0 z-10 mx-auto flex w-full max-w-screen-2xl items-center justify-between bg-white px-4 py-2 shadow-header 2xl:top-4 2xl:rounded-xl"
+  >
     <RouterLink :to="rootPath">
       <img class="hidden h-8 w-auto md:block" src="/identity/pasaditaLogo.svg" />
       <img class="block h-8 w-auto md:hidden" src="/identity/pasaditaLogoMin.svg" />
@@ -9,9 +10,12 @@
     <div class="flex items-center gap-1">
       <div
         class="relative flex cursor-pointer items-center gap-1 rounded-lg p-2 text-blue-400 transition-all hover:bg-shades-100"
-        :class="showMenu ? '' : 'active:scale-95'" @click="toggleMenu">
+        :class="showMenu ? '' : 'active:scale-95'"
+        @click="toggleMenu"
+      >
         <div
-          class="flex size-10 shrink-0 items-center justify-center rounded-full border-2 border-pasadita-blue-2 bg-white">
+          class="flex size-10 shrink-0 items-center justify-center rounded-full border-2 border-pasadita-blue-2 bg-white"
+        >
           <VueFeather type="user" size="25" stroke="#004f82" stroke-width="1.5"></VueFeather>
         </div>
 
@@ -23,25 +27,42 @@
         </div>
 
         <div class="flex items-center">
-          <VueFeather type="chevron-down" size="25" stroke="#004f82" stroke-width="1.5"></VueFeather>
+          <VueFeather
+            type="chevron-down"
+            size="25"
+            stroke="#004f82"
+            stroke-width="1.5"
+          ></VueFeather>
         </div>
 
         <div v-if="showMenu" class="absolute right-0 mt-48 w-48 rounded-b-lg bg-white shadow-xl">
           <ul>
-            <RouterLink class="block cursor-pointer px-4 py-2 transition-all hover:bg-gray-100 active:scale-95"
-              :to="`${rootPath}/configuraciones`">
+            <RouterLink
+              class="block cursor-pointer px-4 py-2 transition-all hover:bg-gray-100 active:scale-95"
+              :to="`${rootPath}/configuraciones`"
+            >
               <VueFeather type="edit-2" size="14" stroke="#004f82" stroke-width="1.5"></VueFeather>
               Modificar
             </RouterLink>
 
-            <RouterLink class="block cursor-pointer px-4 py-2 transition-all hover:bg-gray-100 active:scale-95"
-              :to="`${rootPath}/configuraciones`">
-              <VueFeather VueFeather type="settings" size="14" stroke="#004f82" stroke-width="1.5"></VueFeather>
+            <RouterLink
+              class="block cursor-pointer px-4 py-2 transition-all hover:bg-gray-100 active:scale-95"
+              :to="`${rootPath}/configuraciones`"
+            >
+              <VueFeather
+                VueFeather
+                type="settings"
+                size="14"
+                stroke="#004f82"
+                stroke-width="1.5"
+              ></VueFeather>
               Configuraciones
             </RouterLink>
 
-            <RouterLink class="block cursor-pointer px-4 py-2 transition-all hover:bg-gray-100 active:scale-95"
-              to="/auth/logout">
+            <RouterLink
+              class="block cursor-pointer px-4 py-2 transition-all hover:bg-gray-100 active:scale-95"
+              to="/auth/logout"
+            >
               <VueFeather type="log-out" size="14" stroke="#004f82" stroke-width="1.5"></VueFeather>
               Cerrar Sesión
             </RouterLink>
@@ -51,7 +72,8 @@
 
       <RouterLink
         class="grid size-10 place-items-center gap-2 rounded-md text-blue-400 transition-all hover:bg-shades-100 active:scale-95"
-        :to="`${rootPath}/configuraciones`">
+        :to="`${rootPath}/configuraciones`"
+      >
         <VueFeather type="settings" size="25" stroke-width="1.5"></VueFeather>
       </RouterLink>
     </div>
