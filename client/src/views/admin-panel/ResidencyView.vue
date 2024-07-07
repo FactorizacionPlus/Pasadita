1c381a6615efd886ab660e207ff61c99bc973e75
LeoCorea <00542724@uca.edu.sv>
2024-07-07 13:03:30 -0600

    Implements connection with database and searchbar functionality

--- client/src/views/admin-panel/ResidencyView.vue
+++ client/src/views/admin-panel/ResidencyView.vue
@@ -8,6 +8,7 @@ import SearchBar from "@/components/SearchBar.vue";
 import ModalAdd from "@/components/Modal/Residence/ManageResidence.vue";
 import PaginationItem from "@/components/PaginationItem.vue";
 import { getResidence } from "@/composables/useResidence";
+import { matchSearch } from "@/utils/matchSearch";
 const residence = ref<Residence[]>([]);
 const modalAdd = ref<typeof ModalAdd>();
 
@@ -16,6 +17,11 @@ enum Message {
   CREATE_RESIDENCE = "Crear Residencia",
 }
 
+const searchText = ref("");
+const hideNoResults = ref(false);
+const fieldsToSearch = ["description"];
+
+
 onMounted(async () => {
   await fetchResidence();
 });
@@ -23,12 +29,9 @@ onMounted(async () => {
 async function fetchResidence() {
   const { data } = await getResidence();
   const record = data.value;
-  console.log(record)
   if (!record || !record.ok) return;
   residence.value = record.data ?? [];
 }
-
-console.log(residence.value);
 
 /*const residenceList: Residence[] = [
   {
@@ -52,23 +55,25 @@ console.log(residence.value);
 <template>
   <CurrentPageInfo :title="Message.TITLE" icon="home">
     <button
-      class="inline-flex items-center rounded-lg bg-white p-2 text-center text-sm font-normal text-blue-400 transition-all hover:rounded-xl hover:bg-shades-100 active:scale-95"
-      @click="modalAdd?.show()"
-    >
+      class="flex justify-center rounded-lg bg-white p-2 text-center text-sm font-normal text-blue-400 transition-all hover:rounded-xl hover:bg-shades-100 active:scale-95"
+      @click="modalAdd?.show()">
       <VueFeather type="plus" stroke-width="2.5" size="16"></VueFeather>
       <span>{{ Message.CREATE_RESIDENCE }}</span>
     </button>
   </CurrentPageInfo>
   <article class="flex w-full flex-col gap-8 rounded-lg bg-white p-4">
-    <SearchBar />
+    <SearchBar @search="searchText = $event" @toggle-no-results="hideNoResults = $event" />
     <ul class="grid w-full gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-      <Residences
-        v-for="(residence, index) in residence"
-        :key="index"
-        :residence="residence"
-      />
+      <Residences :class="{
+          'animate-scale-up border-2 border-blue-400':
+            matchSearch(residence, searchText, fieldsToSearch) && searchText.length > 2,
+          hidden:
+            !matchSearch(residence, searchText, fieldsToSearch) &&
+            hideNoResults &&
+            searchText.length > 2,
+        }" :residence="residence" v-for="(residence, index) in residence" :key="index"  />
     </ul>
-    <PaginationItem v-bind="page" v-model="pagination.page" v-if="page" />
+    <!--<PaginationItem v-bind="page" v-model="pagination.page" v-if="page" />-->
     <ModalAdd ref="modalAdd" />
   </article>
 </template>
