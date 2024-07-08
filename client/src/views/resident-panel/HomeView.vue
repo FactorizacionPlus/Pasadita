4c1e7b509923df92c94c90274db4eed9df6dfe53
LeoCorea <00542724@uca.edu.sv>
2024-07-07 19:27:41 -0600

    Added a QR key in databse connection

--- client/src/views/resident-panel/HomeView.vue
+++ client/src/views/resident-panel/HomeView.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
 import SquareButton from "@/components/SquareButton.vue";
+import ModalQR from "@/components/Modal/Resident/LlaveQR.vue";
+import { ref, onMounted } from "vue";
+import type Residence from "@/types/Residence";
 import type { SquareButtonIconsType } from "@/types/SquareButtonIconsType";
-
+import { getOwnResidence } from "@/composables/useResidence";
+const residence = ref<Residence>();
 const RESIDENT_ROOT = "/residente/";
+const modalQR = ref<InstanceType<typeof ModalQR>>();
+
+onMounted(async () => {
+  await fetchResidence();
+});
 
 interface RouteProp {
   title: string;
   icon: SquareButtonIconsType;
   href: string;
+}
+
+
+async function fetchResidence() {
+  const { data } = await getOwnResidence();
+  const record = data.value;
+  residence.value = record?.data;
+  console.log(record);
 }
 
 const routes: RouteProp[] = [
@@ -21,13 +38,9 @@ const routes: RouteProp[] = [
 
 <template>
   <section class="flex size-full items-center justify-center gap-4">
-    <SquareButton
-      type="link"
-      :icon="item.icon"
-      :title="item.title"
-      :href="RESIDENT_ROOT + item.href"
-      :key="index"
-      v-for="(item, index) in routes"
-    />
+    <SquareButton type="link" :icon="item.icon" :title="item.title" :href="RESIDENT_ROOT + item.href" :key="index"
+      v-for="(item, index) in routes" />
+    <SquareButton type="button" icon="custom-phone-qr" title="Generar llave QR" @click="modalQR?.show()" />
   </section>
+  <ModalQR v-if="residence" :residence="residence" ref="modalQR" />
 </template>
